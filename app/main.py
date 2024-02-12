e434b07190b29ccdbf13bc028024c9320c9d4349
Hyeonjae.kim <hyeonjae.dev@gmail.com>
2024-02-12 15:21:45 +0900

    feat: Add routers for instructions and safezones
    Include routers in main.py

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from models.db import mongodb
+from routers import instruction, safezone
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -10,3 +11,6 @@ async def lifespan(app: FastAPI):
     mongodb.close()
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(instruction.router)
+app.include_router(safezone.router)
